d4c2eef31f5449e18734e0f33dff0c5fbc0e6c90
mooc-codes <mooc.codes@gmail.com>
2023-04-22 06:48:20 +0000

    Make KDtree 3D

--- src/utils/kdtree.h
+++ src/utils/kdtree.h
@@ -31,7 +31,7 @@ struct KdTree
 
 		else
 		{
-			int d = depth % 2;
+			int d = depth % 3; //3D point so we have to alternate 3 dims
 			if(point[d] > (*node)->point[d])
 			{
 				insertNode(&((*node)->right), depth + 1, point, id);
@@ -56,16 +56,18 @@ struct KdTree
 		std::vector<float> current = node->point;  
 		std::vector<float> xLims{target[0] - distanceTol, target[0] + distanceTol};
 		std::vector<float> yLims{target[1] - distanceTol, target[1] + distanceTol};
-
+		std::vector<float> zLims{target[2] - distanceTol, target[2] + distanceTol};
 		bool x_within_bounds = current[0] >= xLims[0] && current[0] <= xLims[1];
 		bool y_within_bounds = current[1] >= yLims[0] && current[1] <= yLims[1];
+		bool z_within_bounds = current[2] >= zLims[0] && current[2] <= zLims[1];
 		// Check if current node's point is within the box
-		if (x_within_bounds && y_within_bounds)
+		if (x_within_bounds && y_within_bounds && z_within_bounds)
 		{
 			//Compute distance of point from target.
 			float dx = (target[0] - current[0]) * (target[0] - current[0]);
 			float dy = (target[1] - current[1]) * (target[1] - current[1]);
-			float dist = sqrt( dx + dy );
+			float dz = (target[2] - current[2]) * (target[2] - current[2]);
+			float dist = sqrt( dx + dy + dz);
 			if(dist <= distanceTol)
 			{
 				ids.push_back(node->id);
@@ -73,7 +75,7 @@ struct KdTree
 		}
 
 		// If current node is out of box
-		int dim = depth % 2; // The dimension to be compared.
+		int dim = depth % 3; // The dimension to be compared.
 		if((target[dim] - distanceTol) < current[dim])
 		{
 			search(ids, target, distanceTol, node->left, depth+1);
